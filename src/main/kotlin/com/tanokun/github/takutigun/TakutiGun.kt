28f2c0581f46d16e605a8086ead3ec39ef7f91f3
tanoKun <76431283+tanokun@users.noreply.github.com>
2023-10-07 15:50:38 +0900

    init

---
+++ src/main/kotlin/com/tanokun/github/takutigun/TakutiGun.kt
@@ -0,0 +1,11 @@
+package com.tanokun.github.takutigun
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TakutiGun : JavaPlugin() {
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+    }
+}
